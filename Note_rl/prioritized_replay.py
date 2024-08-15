a5ce090cca6a4503f5dbc325965d6995af4b55c2
NoteDance <notedance@outlook.com>
2024-08-15 22:35:14 +0800

    Update prioritized_replay.py

--- Note_rl/prioritized_replay.py
+++ Note_rl/prioritized_replay.py
@@ -9,7 +9,7 @@ class pr:
     
     def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
         p=(self.TD[1:]+epsilon)**alpha/np.sum((self.TD[1:]+epsilon)**alpha)
-        self.index=np.random.choice(np.arange(len(state_pool),dtype=np.int8),size=[batch],p=p)
+        self.index=np.random.choice(np.arange(len(state_pool)),size=[batch],p=p)
         return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
     
     
