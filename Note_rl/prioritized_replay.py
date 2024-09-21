7e558a6b26d0d1c4916272296914ee287594f856
NoteDance <notedance@outlook.com>
2024-09-21 22:20:23 +0800

    Update prioritized_replay.py

--- Note_rl/prioritized_replay.py
+++ Note_rl/prioritized_replay.py
@@ -1,19 +1,54 @@
+import tensorflow as tf
 import numpy as np
 
 
 class pr:
     def __init__(self):
-        self.TD=np.array(0)
+        self.TD=tf.Variable(7.)
         self.index=None
     
     
     def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
-        p=(self.TD[1:]+epsilon)**alpha/np.sum((self.TD[1:]+epsilon)**alpha)
+        if self.pool_network==True:
+            TD=self.TD[7]
+        else:
+            TD=self.TD
+        p=(TD+epsilon)**alpha/tf.reduce_sum((TD+epsilon)**alpha)
+        self.index=np.random.choice(np.arange(len(state_pool)),size=[batch],p=p.numpy())
+        return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
+    
+    
+    def update_TD(self,TD):
+        if self.pool_network==True:
+            for i in range(len(self.index)):
+                self.TD[7][self.index[i]].assign(TD[i])
+        else:
+            for i in range(len(self.index)):
+                self.TD[self.index[i]].assign(TD[i])
+        return
+
+
+class pr_:
+    def __init__(self):
+        self.TD=7.
+        self.index=None
+    
+    
+    def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
+        if self.pool_network==True:
+            TD=self.TD[7]
+        else:
+            TD=self.TD
+        p=(TD+epsilon)**alpha/np.sum((TD+epsilon)**alpha)
         self.index=np.random.choice(np.arange(len(state_pool)),size=[batch],p=p)
         return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
     
     
     def update_TD(self,TD):
-        for i in range(len(self.index)):
-            self.TD[1:][self.index[i]]=TD[i]
+        if self.pool_network==True:
+            for i in range(len(self.index)):
+                self.TD[7][self.index[i]]=TD[i]
+        else:
+            for i in range(len(self.index)):
+                self.TD[self.index[i]]=TD[i]
         return
