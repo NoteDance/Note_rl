55ca7f282e821d54cf0d7395e6b78234af634e70
NoteDance <notedance@outlook.com>
2025-09-18 14:55:17 +0800

    Update PPO_pr.py

--- Note_rl/examples/keras/pool_network/PPO_pr.py
+++ Note_rl/examples/keras/pool_network/PPO_pr.py
@@ -73,7 +73,7 @@ class PPO(RL):
     
 #    def batch_size_fn(self):
 #        if self.batch_counter%self.update_batches==0:
-#            return self.adabatch(32,7)
+#            return self.adabatch(7)
 #        if self.prepare_flag==True:
 #            return self.batch
     
@@ -143,7 +143,7 @@ class PPO_(RL):
     
 #    def batch_size_fn(self):
 #        if self.batch_counter%self.update_batches==0:
-#            return self.adabatch(32,7)
+#            return self.adabatch(7)
 #        if self.prepare_flag==True:
 #            return self.batch
     
