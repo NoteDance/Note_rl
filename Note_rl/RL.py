8105e3c03e5944633378b2580b075f83d15b87f9
NoteDance <notedance@outlook.com>
2025-12-23 16:07:15 +0800

    Update RL.py

--- Note_rl/RL.py
+++ Note_rl/RL.py
@@ -43,7 +43,7 @@ class RL:
         self.total_time=0
     
     
-    def set(self,policy=None,noise=None,pool_size=None,batch=None,num_updates=None,num_steps=None,update_batches=None,update_steps=None,trial_count=None,criterion=None,PPO=False,HER=False,TRL=False,MARL=False,PR=False,IRL=False,initial_ratio=1.0,initial_TD=7.,lambda_=0.5,alpha=0.7,max_batch=None):
+    def set(self,policy=None,noise=None,pool_size=None,batch=None,num_updates=None,num_steps=None,update_batches=None,update_steps=None,trial_count=None,criterion=None,PPO=False,HER=False,TRL=False,MARL=False,PR=False,IRL=False,initial_ratio=1.0,initial_TD=7.,lambda_=0.5,alpha=0.7):
         self.policy=policy
         self.noise=noise
         self.pool_size=pool_size
@@ -67,19 +67,12 @@ class RL:
                 self.prioritized_replay.PPO=PPO
                 self.prioritized_replay.ratio=self.initial_ratio
                 self.prioritized_replay.TD=self.initial_TD
-                if max_batch is not None:
-                    self.prioritized_replay.ratio_=tf.Variable(tf.zeros([max_batch]))
-                    self.prioritized_replay.TD_=tf.Variable(tf.zeros([max_batch]))
-                else:
-                    self.prioritized_replay.ratio_=tf.Variable(tf.zeros([batch]))
-                    self.prioritized_replay.TD_=tf.Variable(tf.zeros([batch]))
+                self.prioritized_replay.ratio_=tf.Variable(tf.zeros([batch]))
+                self.prioritized_replay.TD_=tf.Variable(tf.zeros([batch]))
                 self.prioritized_replay.batch=tf.Variable(tf.zeros((),dtype=tf.int32))
             else:
                 self.prioritized_replay.TD=self.initial_TD
-                if max_batch is not None:
-                    self.prioritized_replay.TD_=tf.Variable(tf.zeros([max_batch]))
-                else:
-                    self.prioritized_replay.TD_=tf.Variable(tf.zeros([batch]))
+                self.prioritized_replay.TD_=tf.Variable(tf.zeros([batch]))
                 self.prioritized_replay.batch=tf.Variable(tf.zeros((),dtype=tf.int32))
         self.lambda_=lambda_
         self.alpha=alpha
